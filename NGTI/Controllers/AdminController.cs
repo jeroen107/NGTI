327a161a995acf46f4c8722eaf5a1cb88ca810c0
asparagus <60707927+proteanbean@users.noreply.github.com>
2020-11-01 21:59:37 +0100

    Commit Delete controller/view

--- NGTI/Controllers/AdminController.cs
+++ NGTI/Controllers/AdminController.cs
@@ -13,7 +13,9 @@ namespace NGTI.Controllers
 {
     public class AdminController : Controller
     {
+        //sql connection var
         string connectionString = "Server=(localdb)\\mssqllocaldb;Database=NGTI;Trusted_Connection=True;MultipleActiveResultSets=true";
+
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -21,17 +23,40 @@ namespace NGTI.Controllers
         }
         public IActionResult Reservations()
         {
-            //var reservationList = new List<SoloReservation>()
-            //{
-            //    new SoloReservation() { IdSoloReservation = 1, Name = "john", Date = new DateTime(2020,10,27), StartTime = new DateTime(2020,10,27,9,30,0), EndTime = new DateTime(2020,10,27,15,0,0), Reason = "", TableId = 1, Table = null},
-            //    new SoloReservation() { IdSoloReservation = 2, Name = "sponge", Date = new DateTime(2020,10,27), StartTime = new DateTime(2020,10,27,9,30,0), EndTime = new DateTime(2020,10,27,20,0,0), Reason = "", TableId = 2, Table = null},
-            //    new SoloReservation() { IdSoloReservation = 3, Name = "bob", Date = new DateTime(2020,10,27), StartTime = new DateTime(2020,10,27,9,30,0), EndTime = new DateTime(2020,10,27,18,0,0), Reason = "", TableId = 5, Table = null},
-            //   new SoloReservation() { IdSoloReservation = 4, Name = "patrick", Date = new DateTime(2020,10,28), StartTime = new DateTime(2020,10,28,9,30,0), EndTime = new DateTime(2020,10,28,15,0,0), Reason = "", TableId = 3, Table = null}
-            //};
+            // Sql connection
             SqlConnection conn = new SqlConnection(connectionString);
             string sql = "SELECT * FROM SoloReservations";
             SqlCommand cmd = new SqlCommand(sql, conn);
             var model = new List<SoloReservation>();
+            conn.Open();
+            using (conn)
+            {
+                //read all SoloReservations and add them to List<SoloReservation>
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var soloRes = new SoloReservation();
+                    soloRes.IdSoloReservation = (int)rdr["IdSoloReservation"];
+                    soloRes.Name = (string)rdr["Name"];
+                    soloRes.Date = (DateTime)rdr["Date"];
+                    soloRes.StartTime = (DateTime)rdr["StartTime"];
+                    soloRes.EndTime = (DateTime)rdr["EndTime"];
+                    soloRes.Reason = (string)rdr["Reason"];
+                    soloRes.TableId = (int)rdr["TableId"];
+                    model.Add(soloRes);
+                }
+            }
+            conn.Close();
+            return View(model);
+        }
+
+        // GET: /<controller>/
+        public IActionResult Delete(int id)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            string sql = "SELECT * FROM SoloReservations WHERE IdSoloReservation = "+ id;
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            var model = new SoloReservation();
             conn.Open();
             using (conn)
             {
@@ -40,27 +65,31 @@ namespace NGTI.Controllers
                 {
                     var soloRes = new SoloReservation();
                     soloRes.IdSoloReservation = (int)rdr["IdSoloReservation"];
-                    System.Diagnostics.Debug.WriteLine("id = ", soloRes.IdSoloReservation);
                     soloRes.Name = (string)rdr["Name"];
-                    System.Diagnostics.Debug.WriteLine("name = ", soloRes.Name);
                     soloRes.Date = (DateTime)rdr["Date"];
                     soloRes.StartTime = (DateTime)rdr["StartTime"];
                     soloRes.EndTime = (DateTime)rdr["EndTime"];
-                    System.Diagnostics.Debug.WriteLine("EndTime = ", soloRes.EndTime);
                     soloRes.Reason = (string)rdr["Reason"];
                     soloRes.TableId = (int)rdr["TableId"];
-                    model.Add(soloRes);
-
-                    System.Diagnostics.Debug.WriteLine(model.Count());
+                    model = soloRes;
                 }
             }
             conn.Close();
             return View(model);
         }
-
-        public IActionResult Delete()
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
         {
-            return View();
+            SqlConnection conn = new SqlConnection(connectionString);
+            string sql = "DELETE FROM SoloReservations WHERE IdSoloReservation = " + id;
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            conn.Open();
+            using (conn)
+            {
+                SqlDataReader rdr = cmd.ExecuteReader();
+            }
+            conn.Close();
+            return RedirectToAction("Reservations");
         }
     }
 }
