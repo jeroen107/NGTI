c57344537e6fc7c2c625ef7000fcd720bd05fc4b
0989115 <55788313+0989115@users.noreply.github.com>
2020-11-01 12:30:56 +0100

    Update Table.cs

--- NGTI/Table.cs
+++ NGTI/Table.cs
@@ -3,5 +3,19 @@ namespace NGTI
     public class Table
     {
         
+        public int tableNr;
+        public int chairs;
+        public bool taken;
+        public Table(int TableNR, int Chairs)
+        {
+            this.tableNr = TableNR;
+            this.chairs = Chairs;
+            this.taken = false;
+        }
+
+        public void clear()
+        {
+            taken = false;
+        }
     }
 }
