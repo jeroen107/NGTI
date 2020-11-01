a5f502db9714b3852ece19f4424d37ed207d3086
jeroen107 <38460108+jeroen107@users.noreply.github.com>
2020-11-01 12:26:23 +0100

    solo reservations create

--- NGTI/Models/SoloReservation.cs
+++ NGTI/Models/SoloReservation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace NGTI.Models
     public class SoloReservation
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdSoloReservation { get; set; }
         public string Name { get; set; }
         public DateTime Date { get; set; }
