e117ecb2049e2ff7946375494836d2b520f255c8
asparagus <60707927+proteanbean@users.noreply.github.com>
2020-11-02 16:28:14 +0100

    Groupreservations appear in admin overzicht

--- NGTI/Controllers/AdminController.cs
+++ NGTI/Controllers/AdminController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using Microsoft.AspNetCore.Mvc;
 using NGTI.Models;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore.Storage;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,30 +28,57 @@ namespace NGTI.Controllers
             // Sql connection
             SqlConnection conn = new SqlConnection(connectionString);
             string sql = "SELECT * FROM SoloReservations ORDER BY Date ASC";
-            SqlCommand cmd = new SqlCommand(sql, conn);
-            var model = new List<SoloReservation>();
+            string sql2 = "SELECT * FROM GroupReservations ORDER BY Date ASC";
+            string[] sqls = new string[2] { sql, sql2 };
+
+            var solo = new List<SoloReservation>();
+            var group = new List<GroupReservation>();
+
             conn.Open();
             using (conn)
             {
-                //read all SoloReservations and add them to List<SoloReservation>
-                SqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    var soloRes = new SoloReservation();
-                    soloRes.IdSoloReservation = (int)rdr["IdSoloReservation"];
-                    soloRes.Name = (string)rdr["Name"];
-                    soloRes.Date = (DateTime)rdr["Date"];
-                    soloRes.StartTime = (DateTime)rdr["StartTime"];
-                    soloRes.EndTime = (DateTime)rdr["EndTime"];
-                    soloRes.Reason = (string)rdr["Reason"];
-                    soloRes.TableId = (int)rdr["TableId"];
-                    model.Add(soloRes);
-                }
+                //read all reservations and add them to tuple<solo,group>
+                for (int x = 0; x < 2; x++) {
+                    SqlCommand cmd = new SqlCommand(sqls[x], conn);
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    if(x == 0)
+                    {
+                        while (rdr.Read())
+                        {
+                            var res = new SoloReservation();
+                            res.IdSoloReservation = (int)rdr["IdSoloReservation"];
+                            res.Name = (string)rdr["Name"];
+                            res.Date = (DateTime)rdr["Date"];
+                            res.StartTime = (DateTime)rdr["StartTime"];
+                            res.EndTime = (DateTime)rdr["EndTime"];
+                            res.Reason = (string)rdr["Reason"];
+                            res.TableId = (int)rdr["TableId"];
+                            solo.Add(res);
+                        }      
+                    }
+                    else if(x == 1)
+                    {
+                        while (rdr.Read())
+                        {
+                            var res = new GroupReservation();
+                            res.IdGroupReservation = (int)rdr["IdGroupReservation"];
+                            res.Teamname = (string)rdr["Teamname"];
+                            res.Name = (string)rdr["Name"];
+                            res.Date = (DateTime)rdr["Date"];
+                            res.StartTime = (DateTime)rdr["StartTime"];
+                            res.EndTime = (DateTime)rdr["EndTime"];
+                            res.Reason = (string)rdr["Reason"];
+                            res.TableId = (int)rdr["TableId"];
+                            group.Add(res);
+                        }
+                    }
+                }             
             }
             conn.Close();
+            var model = new ReservationsViewModel() { soloList = solo, groupList = group };
             return View(model);
         }
-        public IActionResult Details(int id)
+        public IActionResult Details(int id, string type)
         {
             SqlConnection conn = new SqlConnection(connectionString);
             string sql = "SELECT * FROM SoloReservations WHERE IdSoloReservation = " + id;
@@ -78,7 +107,7 @@ namespace NGTI.Controllers
 
         
         // GET: /<controller>/
-        public IActionResult Delete(int id)
+        public IActionResult Delete(int id, string type)
         {
             SqlConnection conn = new SqlConnection(connectionString);
             string sql = "SELECT * FROM SoloReservations WHERE IdSoloReservation = "+ id;
@@ -106,7 +135,7 @@ namespace NGTI.Controllers
         }
         // POST: /<controller>/
         [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int id)
+        public IActionResult DeleteConfirmed(int id, int type)
         {
             SqlConnection conn = new SqlConnection(connectionString);
             string sql = "DELETE FROM SoloReservations WHERE IdSoloReservation = " + id;
