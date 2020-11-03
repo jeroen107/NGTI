00e3345dd423c58d6fd99674b47ad96f35037bb9
jeroen107 <38460108+jeroen107@users.noreply.github.com>
2020-11-03 20:09:54 +0100

    Soloreservation broken

---
+++ NGTI/Data/Migrations/20201103190802_Soloreservation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NGTI.Data.Migrations
+{
+    public partial class Soloreservation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "SoloReservations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "SoloReservations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
