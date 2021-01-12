c27f973e6e4bbf3fed605e1090bbe7e0a5568aa0
Ertugrul2070 <e.karaduman2070@gmail.com>
2021-01-12 22:01:24 +0100

    wads

--- NGTI/Controllers/HomeController.cs
+++ NGTI/Controllers/HomeController.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NGTI.Models;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NGTI.Data;
 
 namespace NGTI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        string connectionString = "Server=(localdb)\\mssqllocaldb;Database=NGTI;Trusted_Connection=True;MultipleActiveResultSets=true";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -20,7 +26,57 @@ namespace NGTI.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            // Sql connection
+            SqlConnection conn = new SqlConnection(connectionString);
+            string sql = "SELECT * FROM SoloReservations ORDER BY Date ASC";
+            string sql2 = "SELECT * FROM GroupReservations ORDER BY Date ASC";
+            string[] sqls = new string[2] { sql, sql2 };
+
+            var solo = new List<SoloReservation>();
+            var group = new List<GroupReservation>();
+
+            conn.Open();
+            using (conn)
+            {
+                //read all reservations and add them to tuple<solo,group>
+                for (int x = 0; x < 2; x++)
+                {
+                    SqlCommand cmd = new SqlCommand(sqls[x], conn);
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    if (x == 0)
+                    {
+                        while (rdr.Read())
+                        {
+                            var res = new SoloReservation();
+                            res.IdSoloReservation = (int)rdr["IdSoloReservation"];
+                            res.Name = (string)rdr["Name"];
+                            res.Date = (DateTime)rdr["Date"];
+                            res.TimeSlot = (string)rdr["TimeSlot"];
+                            res.Reason = (string)rdr["Reason"];
+                            res.TableId = (int)rdr["TableId"];
+                            solo.Add(res);
+                        }
+                    }
+                    else if (x == 1)
+                    {
+                        while (rdr.Read())
+                        {
+                            var res = new GroupReservation();
+                            res.IdGroupReservation = (int)rdr["IdGroupReservation"];
+                            res.Teamname = (string)rdr["Teamname"];
+                            res.Name = (string)rdr["Name"];
+                            res.Date = (DateTime)rdr["Date"];
+                            res.TimeSlot = (string)rdr["TimeSlot"];
+                            res.Reason = (string)rdr["Reason"];
+                            res.TableId = (int)rdr["TableId"];
+                            group.Add(res);
+                        }
+                    }
+                }
+            }
+            conn.Close();
+            var model = new ReservationsViewModel() { soloList = solo, groupList = group };
+            return View(model);
         }
 
         public IActionResult Privacy()
